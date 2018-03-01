d9eb1a2254ffe59c6982dd875867f8d83c559ecc
Trevor Seitz <trevinla@gmail.com>
2018-03-01 21:59:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,27 +1,12 @@
-# def fizzbuzz(num)
-#   if (num % 3 == 0) && (num % 5 == 0)
-#     puts "FizzBuzz"
-#   elsif num % 3 == 0
-#     puts "Fizz"
-#   elsif num % 5 == 0
-#     puts "Buzz"
-#   else
-#     puts nil
-#   end
-# end
-
-def fizzbuzz(max)
-  arr = []
-  (1..max).each do |n|
-    if ((n % 3 == 0) && (n % 5 == 0))
-      arr << "FizzBuzz"
-    elsif (n % 3 == 0)
-      arr << "Fizz"
-    elsif (n % 5 == 0)
-      arr << "Buzz"
-    else
-      arr << n
-    end
+def fizzbuzz(num)
+  if (num % 3 == 0) && (num % 5 == 0)
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts ('Fizz')
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts nil
   end
-  return arr
 end
+
