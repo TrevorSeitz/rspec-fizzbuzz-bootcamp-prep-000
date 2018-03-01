31ab567fc1d9e9633750deb724d6314bd0abbd12
Trevor Seitz <trevinla@gmail.com>
2018-03-01 21:35:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,13 @@
-# def fizzbuzz(num)
-#   if (num % 3 == 0) && (num % 5 == 0)
-#     puts "FizzBuzz"
-#   elsif num % 3 == 0
-#     puts "Fizz"
-#   elsif num % 5 == 0
-#     puts "Buzz"
-#   else
-#     puts nil
-#   end
-# end
-
-def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
+def fizzbuzz(num)
+  # if (num % 3 == 0) && (num % 5 == 0)
+  #   puts "FizzBuzz"
+  # elsif num % 3 == 0
+  #   puts "Fizz"
+  # elsif num % 5 == 0
+  #   puts "Buzz"
+  # else
+  #   puts nil
+  # end
 end
+
+
