cb654cfb08f542f4d73e2cd36eea7ba8b5627f57
Trevor Seitz <trevinla@gmail.com>
2018-03-01 21:28:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,9 @@ def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
     puts "FizzBuzz"
   elsif num % 3 == 0
-    print "Fizz"
-  else num % 5 == 0
-    "Buzz"
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts nil
   end
